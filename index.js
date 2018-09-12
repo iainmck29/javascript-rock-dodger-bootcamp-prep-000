11063074f1b1756e4b13ee38a1aac5a07823e3d0
Iain McKenzie <iainmck29@hotmail.com>
2018-09-12 20:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -145,11 +145,11 @@ function moveDodgerLeft() {
    */
    var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
-
+    DODGER.style.left = `${left -= 4}px`;
 
    if (left > 1) {
-
-    DODGER.style.left = `${left -= 4}px`;
+     window.requestAnimationFrame(moveDodgerLeft);
+    
 
    }
 
